81bb2448e3d4de85687e6eea1d9e35e7b670f392
Iago Lô <iagooliveiralo03@gmail.com>
2026-02-06 22:28:24 -0300

    testando

--- src/main/java/com/hospital/utils/ConnectionFactory.java
+++ src/main/java/com/hospital/utils/ConnectionFactory.java
@@ -7,11 +7,12 @@ import java.sql.SQLException;
 import java.util.Properties;
 import java.sql.DriverManager;
 
-
+// Classe utilitária para criar conexões com o banco de dados usando as propriedades definidas em db.properties
 public class ConnectionFactory {
 
     private static Properties props;
 
+    // Bloco estático para carregar as propriedades do banco de dados quando a classe for carregada
     static{
         props = new Properties();
         try (InputStream input = ConnectionFactory.class.getClassLoader().getResourceAsStream("db.properties")){
@@ -26,6 +27,7 @@ public class ConnectionFactory {
         }
     }
 
+    // Método para criar e retornar uma nova conexão com o banco de dados usando as propriedades carregadas
     public static Connection createConnection(){
         try {
             return DriverManager.getConnection(
